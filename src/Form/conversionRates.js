258cb21ec71f3e5c3546d9f33c45cb04ce0844df
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-10-28 13:07:24 +0100

    adjust code formatting

--- src/Form/conversionRates.js
+++ src/Form/conversionRates.js
@@ -1,8 +1,8 @@
 const conversionRates = {
-    EUR: 4.34,
-    USD: 4.03,
-    CHF: 4.57,
-    GBP: 4.93,
-  };
+  EUR: 4.34,
+  USD: 4.03,
+  CHF: 4.57,
+  GBP: 4.93,
+};
 
-  export default conversionRates;
+export default conversionRates;
--- src/Result/index.js
+++ src/Result/index.js
@@ -1,4 +1,4 @@
-import "./result.css"
+import "./result.css";
 
 const Result = ({ result }) => {
   return (
