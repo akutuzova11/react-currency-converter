46a5d61d7674f1185e411cc6f4dc4f5f1f4732e3
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-11-12 14:14:03 +0100

    style the app for loading and in case of an error

--- src/Form/formStyled.js
+++ src/Form/formStyled.js
@@ -77,3 +77,68 @@ export const Field = styled.fieldset`
     padding: 20px;
   }
 `;
+
+export const LoadingStatus = styled.div`
+color: ${({ theme }) => theme.colors.primary};
+  padding: 20px;
+  text-align: center;
+  width: 500px;
+  margin: 0 auto;
+  background-color: white;
+  border-radius: 10px;
+  padding-top: 20px;
+  border-top: 10px solid white;
+  font-size: ${({ theme }) => theme.fontSize.medium};
+  font-weight: bold;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+  width: 300px;
+    font-size: ${({ theme }) => theme.fontSize.small};
+  }
+`;
+
+export const LoadingImageStyled = styled.img`
+  width: 100px;
+  height: auto;
+  margin: 20px 0;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    width: 100px;
+  }
+`;
+
+export const ErrorStatus = styled.div`
+  color: ${({ theme }) => theme.colors.red};
+  padding: 20px;
+  text-align: center;
+  width: 100%;
+  max-width: 600px;
+  margin: 0 auto;
+  background-color: white;
+  border-radius: 10px;
+  padding-top: 20px;
+  border-top: 10px solid ${({ theme }) => theme.colors.red};
+  font-size: ${({ theme }) => theme.fontSize.medium};
+  font-weight: bold;
+  display: flex;
+  flex-direction: column; 
+  align-items: center;
+  gap: 10px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    font-size: ${({ theme }) => theme.fontSize.small};
+  }
+`;
+
+export const ErrorImageStyled = styled.img`
+  max-width: 500px;
+  height: auto;
+  margin: 10px 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    width: 300px; 
+  }
+`;
